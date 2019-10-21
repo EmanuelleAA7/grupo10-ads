1a02d9d7ab8c4058dc143238524b094381a6c710
EmanuelleAA7 <emanuelle.araujoa7@gmail.com>
2019-10-21 12:09:15 -0300

    Atualizando servidro node noc e finalizando páginas do serviço

--- noc/public/assets/js/cpu.js
+++ noc/public/assets/js/cpu.js
@@ -4,7 +4,7 @@ var ctxL = document.getElementById("lineChart").getContext('2d');
 var myLineChart = new Chart(ctxL, {
 	type: 'line',
 	data: {
-		labels: ["January", "February", "March", "April", "May", "June", "July","Agosto", "setembro", "out", "nov","dez"],
+		labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho","Agosto", "Setembro", "Outubro", "Novembro","Dezembro"],
 		datasets: [{
 			label: "RAM",
 			 data: [65, 59, 80, 81, 56, 55, 40,50,60,70,50,50],
@@ -15,7 +15,7 @@ var myLineChart = new Chart(ctxL, {
 				'rgba(200, 99, 132, .7)',
 			],
 			borderWidth: 2,
-			// data:[50, 150, 100, 190, 130, 90, 150, 160, 120, 140, 190, 95]
+			data:[55, 50, 80, 90, 40, 75, 51, 60, 44, 55, 80, 95]
 		},
 		{
 			label: "CPU",
@@ -25,6 +25,18 @@ var myLineChart = new Chart(ctxL, {
 			],
 			borderColor: [
 				'rgba(0, 10, 130, .7)',
+			],
+			borderWidth: 2,
+			 data:[58, 50, 70, 90, 35, 92, 56, 60, 74, 60, 80, 95]
+		},
+		{
+			label: "Disco",
+			data: [48, 48, 40, 19, 56, 37, 50,50,70,50,78,57],
+			backgroundColor: [
+				'rgba(0, 192, 96, 1)',
+			],
+			borderColor: [
+				'rgba(0, 124, 48, 1)',
 			],
 			borderWidth: 2,
 			 data:[50, 150, 100, 190, 130, 90, 150, 160, 120, 140, 190, 95]
