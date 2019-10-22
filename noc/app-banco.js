f7ff14850d42673941fd2031cf41d0d1e617c330
EmanuelleAA7 <emanuelle.araujoa7@gmail.com>
2019-10-22 20:07:22 -0300

    Atualizando servidro node noc e finalizando páginas do serviço

---
+++ noc/app-banco.js
@@ -0,0 +1,44 @@
+var desenvolvimento = false;
+
+var configuracoes = {
+    producao: {
+        server: "srvcerberus.database.windows.net",
+        user: "usercerberus",
+        password: "#Gfgrupo10",
+        database: "bdcerberus",
+        options: {
+            encrypt: true
+        },
+        pool: {
+            max: 4,
+            min: 1,
+            idleTimeoutMillis: 30000
+        }
+    },
+    desenvolvimento: {
+        server: "srvcerberus.database.windows.net",
+        user: "usercerberus",
+        password: "#Gfgrupo10",
+        database: "bdcerberus",
+        options: {
+            encrypt: true
+        }
+    }
+}
+ 
+var sql = require('mssql');
+sql.on('error', err => {
+    console.error(`Erro de Conex�o: ${err}`);
+});
+
+var perfil = desenvolvimento ? 'desenvolvimento' : 'producao';
+
+function conectar() {
+  return sql.connect(configuracoes[perfil])
+  // return new sql.ConnectionPool();  
+} 
+
+module.exports = {
+    conectar: conectar,
+    sql: sql
+}
